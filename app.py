91e7ac88b16103d99917f1ab2660ac0f336e9c3f
Mucano Cruz <mucanocruz2@gmail.com>
2024-05-17 21:00:41 +0100

    Implementação de login

--- app.py
+++ app.py
@@ -1,16 +1,60 @@
-from flask import Flask
+from flask import Flask, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import Blueprint, render_template
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/tarefas_db"
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
 @app.route('/')
-def hello():
-    return {"hello": "world"}
+def index():
+    return render_template('index.html')
+
+
+@app.route('/profile')
+def profile():
+    return 'Profile'
+
+#################### LOGIN ################################
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    # login code goes here
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+
+   # user = User.query.filter_by(email=email).first()
+    usuarioModel = UsuarioModel.query.filter_by(
+        usuario=usuario, senha=senha).first()
+
+    if not usuarioModel:
+        flash('Please check your login details and try again.')
+        # return redirect('auth.login')
+        return 'Usuário não encontrado'
+    else:
+        return 'Nome do Usuário: ' + usuarioModel.nome
+    # return 'Teste login post'
+
+
+@app.route('/signup')
+def signup():
+    return 'Signup'
+
+
+@app.route('/logout')
+def logout():
+    return 'Logout'
 
 
 if __name__ == '__main__':
@@ -45,3 +89,29 @@ class EstadoTarefaModel(db.Model):
 
     def __repr__(self):
         return f"<EstadoTarefa {self.nome}>"
+
+
+class TarefaModel(db.Model):
+    __tablename__ = 'tarefas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String())
+    descricao = db.Column(db.String())
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'))
+    estado_id = db.Column(db.Integer, db.ForeignKey('estados_tarefa.id'))
+
+    def __init__(self, titulo, descricao, usuario_id, estado_id):
+        self.titulo = titulo
+        self.descricao = descricao
+        self.usuario_id = usuario_id
+        self.estado_id = estado_id
+
+    def __repr__(self):
+        return f"<Usuario {self.titulo}>"
+
+
+student_course = db.Table(
+    'tarefa_usuario',
+    db.Column('tarefa_id', db.Integer, db.ForeignKey('tarefas.id')),
+    db.Column('usuario_id', db.Integer, db.ForeignKey('usuarios.id'))
+)
---
+++ auth.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+
